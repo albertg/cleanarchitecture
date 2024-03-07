7703f0b82ab0c99b48782eed6b2444abef40d6e5
Albert George <albert.george@gmail.com>
2024-03-07 16:17:38 +0530

    Code cleanup

--- Clean.Architecture.API/Transforms/Interface/IParishnerTransform.cs
+++ Clean.Architecture.API/Transforms/Interface/IParishnerTransform.cs
@@ -6,11 +6,7 @@ namespace Clean.Architecture.API.Transforms.Interface
 {
     public interface IParishnerTransform
     {
-        //GetParishnerInfoResponse Transform(Parishner parishner);
-        //GetParishnerDetailsResponse Transform(Parishner parishner);
         T Transform<T>(Parishner parishner);
-        //List<GetParishnerInfoResponse> Transform(List<Parishner> parishnerList);
-        //List<GetParishnerDetailsResponse> Transform(List<Parishner> parishners);
         List<T> Transform<T>(List<Parishner> parishners);
         Parishner Transform(NewParishnerRequest newParishnerRequest);
         ParishnerType Transform(MemberType memberType);
